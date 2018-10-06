762db75584449e49745f58f9da592c4255b45a70
Tony Tung <tony61507@gmail.com>
2018-10-06 16:48:10 -0700

    2-1, done without testing

--- src/activations.c
+++ src/activations.c
@@ -2,6 +2,22 @@
 #include <math.h>
 #include "uwnet.h"
 
+double logistic_func(x) {
+  return 1 / (1 + exp(-1 * x));
+}
+
+double relu_func(x) {
+  return x > 0 ? x : 0;
+}
+
+double lrelu_func(x) {
+  return x > 0 ? x : 0.1 * x;
+}
+
+double softmax_func(x) {
+  return exp(x);
+}
+
 // Run an activation function on each element in a matrix,
 // modifies the matrix in place
 // matrix m: Input to activation function
@@ -12,20 +28,23 @@ void activate_matrix(matrix m, ACTIVATION a)
     for(i = 0; i < m.rows; ++i){
         double sum = 0;
         for(j = 0; j < m.cols; ++j){
-            double x = m.data[i*m.cols + j];
+            int index = i * m.cols + j;
+            double x = m.data[index];
             if(a == LOGISTIC){
-                // TODO
+                m.data[index] = logistic_func(x);
             } else if (a == RELU){
-                // TODO
+                m.data[index] = relu_func(x);
             } else if (a == LRELU){
-                // TODO
+                m.data[index] = lrelu_func(x);
             } else if (a == SOFTMAX){
-                // TODO
+                m.data[index] = softmax_func(x);
             }
             sum += m.data[i*m.cols + j];
         }
         if (a == SOFTMAX) {
-            // TODO: have to normalize by sum if we are using SOFTMAX
+            for (int k = 0; k < m.cols; k++) {
+                m.data[i * m.cols + k] /= sum;
+            }
         }
     }
 }
